47b1b9e2c95569cb1ad81bd44982b8d312943723
Svenja Dunger <svenjadunger@web.de>
2024-02-02 12:15:51 +0100

    filter optimizing

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -5,35 +5,39 @@ const Searchbar = () => {
   const [query, setQuery] = useState('');
   const [characters, setCharacters] = useState([]);
 
-  const fetchCharacters = async () => {
-    try {
-      const response = await fetch(`https://rickandmortyapi.com/api/character/?name=${query}`);
-      const data = await response.json();
-      setCharacters(data.results);
-    } catch (error) {
-      console.error('Error:', error);
-    }
-  };
+  // this function is called if user types in searchbar
+  const handleInputChange = async (e) => {
+    const input = e.target.value;
+    setQuery(input); // actualizes the searchtext
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    fetchCharacters();
+    // case sensitivity
+    const searchQuery = input.toLowerCase();
+
+    if (searchQuery) {
+      try {
+        const response = await fetch(`https://rickandmortyapi.com/api/character/?name=${searchQuery}`);
+        const data = await response.json();
+        setCharacters(data.results); // stores the search-results
+      } catch (error) {
+        console.error('Error:', error);
+      }
+    } else {
+      setCharacters([]); // empties result-list if searchfield is empty 
+    }
   };
 
   return (
     <div>
-      <form id="searchForm" onSubmit={handleSubmit}>
-        <div className="input-wrapper">
-          <input
-            type="text"
-            placeholder="Type to search..."
-            value={query}
-            onChange={(e) => setQuery(e.target.value)}
-          />
-        </div>
-      </form>
+      <div className="input-wrapper">
+        <input
+          type="text"
+          placeholder="Type to search..."
+          value={query}
+          onChange={handleInputChange} // reacts on changes in searchfield
+        />
+      </div>
       <div id="searchResults" className="d-flex flex-wrap">
-        {characters.map(character => (
+        {characters.map((character) => (
           <div key={character.id} className="flip-card">
             <div className="flip-card-inner">
               <div className="flip-card-front">
